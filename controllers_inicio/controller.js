b07876f1278dcf68fa636f729f011a6e91fb574a
Diiegohdez <diiego.hdez@gmail.com>
2024-05-25 16:42:23 -0500

    actualizacion

--- controllers_inicio/controller.js
+++ controllers_inicio/controller.js
@@ -2,4 +2,19 @@ let app = angular.module("Myapp", []);
 
 app.controller("Mycontroller", function($scope){
     $scope.nombre = "pedro";
+    $scope.nuevoComentario = [];
+    $scope.comentarios = [
+        {
+            comentario: "primer comentario",
+            usuario : "pimer usuario"
+        },
+        {
+            comentario: "segundo comentario",
+            usuario: "segundo usuario"
+        }
+    ]
+    $scope.agregarComenario = function () {
+        $scope.comentarios.push($scope.nuevoComentario);
+        $scope.nuevoComentario = {};
+    }
 });
